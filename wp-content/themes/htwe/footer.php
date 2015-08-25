fb876c5be99d3a4b045298c6c323f44709cdeae6
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-08-25 12:52:25 +0200

    1.0.0

--- wp-content/themes/htwe/footer.php
+++ wp-content/themes/htwe/footer.php
@@ -2,10 +2,10 @@
     <footer id="footer" class="footer">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-md-6">
+          <div class="col-xs-12 col-md-6">
             <?php secondary_nav('secondary-nav','navbar_bottom'); ?>
           </div>
-          <div class="col-md-6">
+          <div class="col-xs-12 col-md-6">
             <div class="footer_attribution">
               <span>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</span>
               <span>Made in <svg class="svg-icon shape-icon-africa"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-icon-africa"></use></svg> by <a href="http://www.plusplusminus.co.za/?utm_source=Aevitas&amp;utm_medium=Footer&amp;utm_campaign=Credit" title="PlusPlusMinus Design &amp; Development" target="_blank">PlusPlusMinus</a></span>
